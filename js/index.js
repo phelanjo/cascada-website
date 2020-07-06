775ff4e56faa6f587d3b662795fa282a85855ea7
Johnny Phelan <phelanjo@oregonstate.edu>
2020-07-06 15:56:44 -0700

    implement edit button on click event to populate form fields

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 $(".delete").click(function() {
   $(this).parent().parent().remove()
 })
+
+$(".edit").click(function() {
+  var td = $(this).closest('tr').find('td');
+  var rows = []
+
+  for(i = 0; i < td.length - 1; i++) {
+    rows.push(td.eq(i).html())
+  }
+
+  $("#edit-form *").filter(':input').each(function(index) {
+    $(this).attr('placeholder', rows[index])
+  })
+})

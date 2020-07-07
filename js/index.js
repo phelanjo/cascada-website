2fb3f5f6d23cafd415c3eae788c89b25482fb17f
Johnny Phelan <phelanjo@oregonstate.edu>
2020-07-07 08:16:16 -0700

    Add GET request to create table of waterfalls from response. Refactor edit/delete button functions to use .on() method.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,45 @@
-$(".delete").click(function() {
-  $(this).parent().parent().remove()
-})
+$("document").ready(function() {
+  $.ajax({
+    url: "http://127.0.0.1:5000/fetch_all/",
+    type: "GET",
+    contentType: "application/json",
+    success: function(response) {
+      var editButton = '<button type="button" class="btn btn-sm edit" data-toggle="modal" data-target="#editModal">Edit</button> '
+      var deleteButton = '<button type="button" class="btn btn-sm delete"><i class="fas fa-trash"></i></button>'
 
-$(".edit").click(function() {
-  var td = $(this).closest('tr').find('td');
-  var rows = []
-
-  for(i = 0; i < td.length - 1; i++) {
-    rows.push(td.eq(i).html())
-  }
-
-  $("#edit-form *").filter(':input').each(function(index) {
-    $(this).attr('placeholder', rows[index])
+      for(i = 0; i < response.length; i++) {
+        $("#waterfall-table-body").append($("<tr></tr>").append(
+          $('<td scope="row"></td>').html(response[i].name),
+          $('<td></td>').html(response[i].height),
+          $('<td></td>').html(response[i].latitude),
+          $('<td></td>').html(response[i].longitude),
+          $('<td></td>').append(editButton, deleteButton)
+        ))
+      }
+    }
   })
+
+  $("#waterfall-table-body").on("click", ".edit", (function() {
+      var td = $(this).closest('tr').find('td');
+      var rows = []
+  
+      for(i = 0; i < td.length - 1; i++) {
+        rows.push(td.eq(i).html())
+      }
+  
+      $("#edit-form *").filter(':input').each(function(index) {
+        $(this).attr('placeholder', rows[index])
+      })
+    })
+  )
+
+  $("#waterfall-table-body").on("click", ".delete", (function() {
+      $(this).parent().parent().remove()
+    })
+  )
+
 })
+
+
+
+
